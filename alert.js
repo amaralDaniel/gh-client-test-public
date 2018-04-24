4c016005b5bc3e9262ee85c5362f0c11954246e9
Daniel Amaral <amaral94daniel@gmail.com>
2018-04-24 20:14:26 +0100

    Upload file test

--- alert.js
+++ alert.js
@@ -1,4 +1,13 @@
-// Insert code here
-<v-alert type="success" :value="true">
-      This is a success alert.   
-</v-alert>
+let _self = this
+      _self.axiosInstance.get('/repos/' + _self.repositoryOwner + '/' + _self.repositoryName + '/branches').then(function (result) {
+        result.data.forEach(function (each) {
+          _self.branches.push({text: each.name, sha: each.commit.sha})
+        })
+      })
+      _self.axiosInstance.get(`/repos/${_self.repositoryOwner}/${_self.repositoryName}/contents/${_self.path}`).then(function (response) {
+        console.log(response)
+        _self.blobSha = response.data.sha
+        _self.code = decodeURIComponent(atob(response.data.content).split('').map(function (c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+        }).join(''))
+      })
